25dcd50442d37daf2dfb2641630628396d81750a
vizorian <birstonasvilius@gmail.com>
2021-09-20 17:43:38 +0300

    Added example solution from Moodle

---
+++ SignalR-GameServer-v1/Hubs/GameHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System;
+
+namespace SignalR_GameServer_v1.Hubs
+{
+    public class GameHub : Hub
+    {
+        public GameHub()
+        {
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            //await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+            //await Clients.Caller.SendAsync("ReceiveMessage", user, "delivered: " + message);
+        }
+    }
+}
