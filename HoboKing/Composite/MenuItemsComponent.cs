dbe313d8a0cb0c63e066106f485b463533fedb1a
vizorian <birstonasvilius@gmail.com>
2021-12-02 09:47:15 +0200

    Cosmetic changes

--- HoboKing/Composite/MenuItemsComponent.cs
+++ HoboKing/Composite/MenuItemsComponent.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Emit;
-using HoboKing.Control;
-using HoboKing.Graphics;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace HoboKing.Composite
 {
@@ -78,86 +73,5 @@ namespace HoboKing.Composite
                 component.Print();
             }
         }
-
-        //private readonly HoboKingGame hoboKingGame;
-        //private readonly Color menuItemColor;
-
-        //private readonly List<IComponent> menuItems;
-
-        //private readonly Vector2 Position;
-        //private readonly Color selectedMenuItemColor;
-        //private readonly int textScale;
-        //public IComponent SelectedMenuItem;
-
-        //public MenuItemsComponent(HoboKingGame hoboKingGame, Vector2 position, Color menuItemColor,
-        //    Color selectedMenuItemColor, int textScale)
-        //    : base(hoboKingGame)
-        //{
-        //    this.hoboKingGame = hoboKingGame;
-        //    this.Position = position;
-        //    this.menuItemColor = menuItemColor;
-        //    this.selectedMenuItemColor = selectedMenuItemColor;
-        //    this.textScale = textScale;
-
-        //    menuItems = new List<IComponent>();
-        //    SelectedMenuItem = null;
-        //}
-
-        //public void AddItem(string text)
-        //{
-        //    // Setting up the position according to the item collection index
-        //    var p = new Vector2(Position.X, Position.Y + menuItems.Count * textScale * 70);
-        //    var item = new MenuItem(text, p);
-        //    menuItems.Add(item);
-
-        //    // Selecting the first item
-        //    SelectedMenuItem ??= item;
-        //}
-
-        //public void SelectNext()
-        //{
-        //    var index = menuItems.IndexOf(SelectedMenuItem);
-        //    if (index < menuItems.Count - 1)
-        //        SelectedMenuItem = menuItems[index + 1] as MenuItem;
-        //    else
-        //        SelectedMenuItem = menuItems[0] as MenuItem;
-        //}
-
-        //public void SelectPrevious()
-        //{
-        //    var index = menuItems.IndexOf(SelectedMenuItem);
-        //    if (index > 0)
-        //        SelectedMenuItem = menuItems[index - 1] as MenuItem;
-        //    else
-        //        SelectedMenuItem = menuItems[^1] as MenuItem;
-        //}
-
-        //public override void Update(GameTime gameTime)
-        //{
-        //    if (InputController.KeyPressed(Keys.Up))
-        //        SelectPrevious();
-        //    if (InputController.KeyPressed(Keys.Down))
-        //        SelectNext();
-        //    base.Update(gameTime);
-        //}
-
-        //public override void Draw(GameTime gameTime)
-        //{
-        //    hoboKingGame.SpriteBatch.Begin();
-
-        //    foreach (var menuItem in menuItems)
-        //    {
-        //        var color = menuItemColor;
-        //        if (menuItem == SelectedMenuItem)
-        //            color = selectedMenuItemColor;
-
-        //        hoboKingGame.SpriteBatch.DrawString(ContentLoader.MenuFont, menuItem., menuItem.GetPosition(), color,
-        //            0.0f, new Vector2(0, 0), textScale, SpriteEffects.None, 0);
-        //    }
-
-        //    hoboKingGame.SpriteBatch.End();
-        //    base.Draw(gameTime);
-        //}
-
     }
 }
